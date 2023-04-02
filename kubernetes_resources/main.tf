7a07ba097107488a6b452d532c76476f4f719793
ildar-agliukov <ildariwe@gmail.com>
2023-04-02 17:44:20 +0600

    add: producer

--- kubernetes_resources/main.tf
+++ kubernetes_resources/main.tf
@@ -8,6 +8,10 @@ module "kafka" {
   source = "./kafka"
 }
 
+module "kafka_producer" {
+  source = "./kafka_producer"
+}
+
 
 # output "lb_ip" {
 #   value = kubernetes_service.control_center.status.0.load_balancer.0.ingress.0.ip
